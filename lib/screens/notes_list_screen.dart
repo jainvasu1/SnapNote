a56a84b49ed7ce17ad65c7525286317a764c09db
jainvasu1 <vasubajaj2001@gmail.com>
2026-06-17 15:52:15 +0530

    style: noteslist screen ui changes.

--- lib/screens/notes_list_screen.dart
+++ lib/screens/notes_list_screen.dart
@@ -1,36 +1,76 @@
 import 'package:flutter/material.dart';
 import '../services/storage_service.dart';
 import '../constants/app_routes.dart';
+import '../models/note.dart';
 
 class NotesListScreen extends StatelessWidget {
   const NotesListScreen({super.key});
 
+  // priority color
+  Color getPriorityColor(String priority) {
+    switch (priority) {
+      case "High":
+        return Colors.red;
+      case "Medium":
+        return Colors.orange;
+      default:
+        return Colors.green;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final notes = StorageService.getNotes();
+    final List<Note> notes = StorageService.getNotes();
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Notes")),
-      body: ListView.builder(
-        itemCount: notes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(notes[index].title),
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                AppRoutes.detail,
-                arguments: notes[index],
-              );
-            },
-          );
-        },
-      ),
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: const Text("Notes"), backgroundColor: Colors.black),
+
+      body: notes.isEmpty
+          ? const Center(
+              child: Text(
+                "No Notes Found",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          : ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                final note = notes[index];
+
+                return Card(
+                  color: Colors.black54,
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: getPriorityColor(note.priority),
+                    ),
+                    title: Text(
+                      note.title,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      note.date,
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        AppRoutes.detail,
+                        arguments: note,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
         onPressed: () {
           Navigator.pushNamed(context, AppRoutes.addNote);
         },
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add, color: Colors.black),
       ),
     );
   }
